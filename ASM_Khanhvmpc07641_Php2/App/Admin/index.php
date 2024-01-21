976c7e89936c653643355edeac2e9f7ccbfaeec7
vo minh khanh <khanhvmpc07641@fpt.edu.vn>
2024-01-21 22:03:38 +0700

    asm1

---
+++ ASM_Khanhvmpc07641_Php2/App/Admin/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords"
+        content="wrappixel, admin dashboard, html css dashboard, web dashboard, bootstrap 5 admin, bootstrap 5, css3 dashboard, bootstrap 5 dashboard, Nice lite admin bootstrap 5 dashboard, frontend, responsive bootstrap 5 admin template, Nice admin lite design, Nice admin lite dashboard bootstrap 5 dashboard template">
+    <meta name="description"
+        content="Nice Admin Lite is powerful and clean admin dashboard template, inpired from Bootstrap Framework">
+    <meta name="robots" content="noindex,nofollow">
+    <title>Nice Admin Lite Template by WrapPixel</title>
+    <link rel="canonical" href="https://www.wrappixel.com/templates/niceadmin-lite/" />
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="Public/assets/images/favicon.png">
+    <!-- Custom CSS -->
+    <link href="Public/assets/libs/chartist/dist/chartist.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="Public/dist/css/style.min.css" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
+
+<body>
+    <?php require 'view/header.php'; ?>
+    <?php require 'view/aside.php'; ?>   
+       
+        <div class="page-wrapper">
+           
+            <?php require 'view/content.php'; ?>
+                
+            </div>
+            
+            <?php require 'view/footer.php'; ?>
+           
+        </div>
+        
+    </div>
+    
+    <script src="Public/assets/libs/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+    <script src="Public/assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- slimscrollbar scrollbar JavaScript -->
+    <script src="Public/assets/extra-libs/sparkline/sparkline.js"></script>
+    <!--Wave Effects -->
+    <script src="Public/dist/js/waves.js"></script>
+    <!--Menu sidebar -->
+    <script src="Public/dist/js/sidebarmenu.js"></script>
+    <!--Custom JavaScript -->
+    <script src="Public/dist/js/custom.min.js"></script>
+    <!--This page JavaScript -->
+    <!--chartis chart-->
+    <script src="Public/assets/libs/chartist/dist/chartist.min.js"></script>
+    <script src="Public/assets/libs/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.min.js"></script>
+    <script src="Public/dist/js/pages/dashboards/dashboard1.js"></script>
+</body>
+
+</html>
---
+++ ASM_Khanhvmpc07641_Php2/App/Views/listUser.php
@@ -0,0 +1,121 @@
+<div class="container-fluid">
+                <!-- ============================================================== -->
+                <!-- Start Page Content -->
+                <!-- ============================================================== -->
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card card-body">
+                            <h4 class="card-title">Default Forms</h4>
+                            <h5 class="card-subtitle"> All bootstrap element classies </h5>
+                            <form class="form-horizontal mt-4">
+                                <div class="form-group">
+                                    <label>Default Text <span class="help"> e.g. "George deo"</span></label>
+                                    <input type="text" class="form-control" value="George deo...">
+                                </div>
+                                <div class="form-group">
+                                    <label for="example-email">Email <span class="help"> e.g.
+                                            "example@gmail.com"</span></label>
+                                    <input type="email" id="example-email" name="example-email" class="form-control"
+                                        placeholder="Email">
+                                </div>
+                                <div class="form-group">
+                                    <label>Password</label>
+                                    <input type="password" class="form-control" value="password">
+                                </div>
+                                <div class="form-group">
+                                    <label>Placeholder</label>
+                                    <input type="text" class="form-control" placeholder="placeholder">
+                                </div>
+                                <div class="form-group">
+                                    <label>Text area</label>
+                                    <textarea class="form-control" rows="5"></textarea>
+                                </div>
+                                <div class="form-group">
+                                    <label>Read only input</label>
+                                    <input class="form-control" type="text" placeholder="Readonly input here…" readonly>
+                                </div>
+                                <div class="form-group">
+                                    <fieldset disabled>
+                                        <label for="disabledTextInput">Disabled input</label>
+                                        <input type="text" id="disabledTextInput" class="form-control"
+                                            placeholder="Disabled input">
+                                    </fieldset>
+                                </div>
+                                <div class="form-group row pt-3">
+                                    <div class="col-sm-4">
+                                        <div class="form-check">
+                                            <input type="checkbox" class="form-check-input" id="customCheck1">
+                                            <label class="form-check-label mb-0" for="customCheck1">Check this custom
+                                                checkbox</label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input type="checkbox" class="form-check-input" id="customCheck2">
+                                            <label class="form-check-label mb-0" for="customCheck2">Check this custom
+                                                checkbox</label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input type="checkbox" class="form-check-input" id="customCheck3">
+                                            <label class="form-check-label mb-0" for="customCheck3">Check this custom
+                                                checkbox</label>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-4">
+                                        <div class="form-check">
+                                            <input type="radio" id="customRadio1" name="customRadio"
+                                                class="form-check-input">
+                                            <label class="form-check-label mb-0" for="customRadio1">Toggle this custom
+                                                radio</label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input type="radio" id="customRadio2" name="customRadio"
+                                                class="form-check-input">
+                                            <label class="form-check-label mb-0" for="customRadio2">Toggle this custom
+                                                radio</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label>Input Select</label>
+                                    <select class="form-select shadow-none col-12" id="inlineFormCustomSelect">
+                                        <option selected>Choose...</option>
+                                        <option value="1">One</option>
+                                        <option value="2">Two</option>
+                                        <option value="3">Three</option>
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <label>Default file upload</label>
+                                    <input type="file" class="form-control">
+                                </div>
+                                <div class="form-group">
+                                    <label>Custom File upload</label>
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text">Upload</span>
+                                        </div>
+                                        <div class="custom-file">
+                                            <input type="file" class="custom-file-input" id="inputGroupFile01">
+                                            <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label>Helping text</label>
+                                    <input type="text" class="form-control" placeholder="Helping text">
+                                    <span class="help-block"><small>A block of help text that breaks onto a new line and
+                                            may extend beyond one line.</small></span> </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <!-- ============================================================== -->
+                <!-- End PAge Content -->
+                <!-- ============================================================== -->
+                <!-- ============================================================== -->
+                <!-- Right sidebar -->
+                <!-- ============================================================== -->
+                <!-- .right-sidebar -->
+                <!-- ============================================================== -->
+                <!-- End Right sidebar -->
+                <!-- ============================================================== -->
+            </div>
